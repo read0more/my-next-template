34f2b9019c3060f2a81e3a0dab975b43f4d2e5ee
read0more <read0more@gmail.com>
2023-04-26 09:41:46 +0900

    :wrench: vitest

--- src/__tests__/Home.test.tsx
+++
@@ -1,7 +0,0 @@
-import { expect, test } from 'vitest';
-import { render, screen, within } from '@testing-library/react';
-import Home from '../pages';
-
-test('home', () => {
-  expect(1).toBe(1);
-});
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      src: '/src', // Yarn link protocol이 제대로 안먹히는 모양이라(src에 package.json 넣으면 되긴 함), alias로 대체
+    },
   },
 });
